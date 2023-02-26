eeb69c3390f522e27dd57c1f91b963bc4b00ac0e
hpekkan <hpekkan@etu.edu.tr>
2023-02-26 17:51:53 +0300

    prediction api ready state

--- api/main.py
+++ api/main.py
@@ -2,10 +2,10 @@
 import uvicorn
 import pickle
 from pydantic import BaseModel
-from fastapi import FastAPI
+from fastapi import Request,FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
-
+import json
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -50,33 +50,49 @@ def read_root():
     return {"data": "Welcome to online employee hireability prediction model"}
 
 class Ticket(BaseModel):
-    Pass_id=int,
-    pclass=int,
-    name=str,
-    sex=str,
-    age=int,
-    sibsp=int,
-    parch=int,
-    ticket=str,
-    fare=float,
-    cabin=str,
+    Pass_id=int
+    pclass=int
+    name=str
+    sex=str
+    age=int
+    sibsp=int
+    parch=int
+    ticket=str
+    fare=float
+    cabin=str
     embarked=str
     
 # Setting up the prediction route
-@app.post("/prediction/")
-async def get_predict(api_data: Ticket):
-    pipeline = data(Pass_id=api_data.Pass_id,pclass=api_data.Pass_id,name=api_data.name,sex=api_data.sex,age=api_data.age,sibsp=api_data.sibsp,parch=api_data.parch,ticket=api_data.ticket,fare=api_data.fare,cabin=api_data.cabin,embarked=api_data.embarked,train_age_median=train_age_median,train_embarked_mode=train_embarked_mode,train_fare_median=train_fare_median)
+@app.get("/prediction")
+async def get_predict(request: Request):
+    api_data = await request.json()
+    
+    pipeline = data(Pass_id=api_data["Pass_id"],pclass=api_data["pclass"],name=api_data["name"],sex=api_data["sex"],age=api_data["age"],sibsp=api_data["sibsp"],parch=api_data["parch"],ticket=api_data["ticket"],fare=api_data["fare"],cabin=api_data["cabin"],embarked=api_data["embarked"],train_age_median=train_age_median,train_embarked_mode=train_embarked_mode,train_fare_median=train_fare_median)
     prepro_data = pipeline.preproccessing()
-    print(model.predict(prepro_data.values.reshape(1, -1)))
     prediction = model.predict(prepro_data.values.reshape(1, -1))
+    
     return {
         "data": {
-            'prediction': str(prediction[0]),
-            
-            #'interpretation': 'Candidate can be hired.' if hired == 1 else 'Candidate can not be hired.'
-        }
+            'prediction': str(prediction[0]) }
     }
 
 # Configuring the server host and port
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
+
+
+    """
+    {
+    "Pass_id" : 1,
+    "pclass" : 3,
+    "name" : "Braund, Mr. Owen Harris",
+    "sex" : "M",
+    "age": 22,
+    "sibsp" : 1,
+    "parch" : 0,
+    "ticket" : "A/5 21171",
+    "fare" : 8.25,
+    "cabin":" ",
+    "embarked":"S"
+}
+    """
