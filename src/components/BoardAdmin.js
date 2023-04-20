555d93eb7e377dbd24a4eec5dbaa636a6b65c0e9
hpekkan <hpekkan@etu.edu.tr>
2023-04-20 14:14:55 +0300

    voyages getting request handled in user.service

--- src/components/BoardAdmin.js
+++ src/components/BoardAdmin.js
@@ -1,17 +1,39 @@
 import React, { useState, useEffect } from "react";
- import Voyage from "./Voyage";
- import "../App.css";
+import Voyage from "./Voyage";
+import "../App.css";
+import UserService from "../services/user.service";
 
 const BoardUser = () => {
-  
+  const [content, setContent] = useState({});
+  useEffect(() => {
+    UserService.getVoyages().then(
+      (response) => {
+        setContent(response.data["Voyages"]);
+        console.log(response.data["Voyages"]);
+      },
+      (error) => {
+        const _content =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+        setContent(_content);
+      }
+    );
+  }, []);
   return (
     <div className="container">
       <header className="jumbotron">
         <h3>Admin Panel</h3>
       </header>
       <div className="voyages">
-        <Voyage route_id={1} departure={"Ankara"} arrival={"İstanbul"} />
-
+        
+        {content.map(route => (
+        
+          <Voyage key={route.route_id} route_id={route.route_id} departure={route.departure_location} arrival={route.arrival_location} />
+        
+      ))}
       </div>
     </div>
   );
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -21,10 +21,24 @@ const getUser = async () => {
   }
   return response;
 };
-
+const getVoyages = async () => {
+  const response = await axios.get(API_URL + "voyages", { headers: authHeader() });
+  if(response.status=== 401 ){
+    AuthService.refresh();
+    const newResponse = await axios.get(API_URL + "voyages", { headers: authHeader() });
+    if(newResponse.status=== 401 ){
+      AuthService.logout();
+    }
+  }else if (response.status=== 403){
+    AuthService.logout();
+    return null;
+  }
+  return response;
+};
 const UserService = {
   getPublicContent,
   getUser,
+  getVoyages,
 };
 
 export default UserService;
